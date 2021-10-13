ee35fe40ecd6e7a95c4d0bfe3f8a716175f7f907
Luis <luisdaniel200926@hotmail.com>
2021-10-13 11:26:30 -0500

    Update,insert projects in project controller And added Routes

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProjectController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/getAllProjects', [ProjectController::class,'getAllProjects']);
+Route::get('/insertNewProject', [ProjectController::class,'insertProject']);
+Route::get('/updateProject', [ProjectController::class,'updateProject']);
+Route::get('/updateInactiveProjects', [ProjectController::class,'updateInactiveProjects']);
